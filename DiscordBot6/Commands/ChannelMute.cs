9256d57aafe660914778dc4ef581dccda3a6359a
Mayall <mayall0513@outlook.com>
2021-10-23 17:46:29 +0100

    Added error handlers (not sure what to do with them yet) and switched over to using server/user extension methods

--- DiscordBot6/Commands/ChannelMute.cs
+++ DiscordBot6/Commands/ChannelMute.cs
@@ -9,61 +9,64 @@ namespace DiscordBot6.Commands {
     public class ChannelMute : ModuleBase<SocketCommandContext> {
         [Command]
         [Summary("Mutes a user in a specific voice channel. With an optional time limit.")]
-        public async Task Command(IUser user, params string[] arguments) {
-            if (Context.Message.Author is SocketGuildUser author) {
-                SocketGuild guild = Context.Guild;
-                SocketGuildUser guildUser = guild.GetUser(user.Id);
+        public async Task Command(IUser user, string[] arguments) {
+            SocketGuild guild = Context.Guild;
+            SocketGuildUser guildUser = guild.GetUser(user.Id);
 
-                if (guildUser != null && guildUser.VoiceChannel != null) {
-                    int days = 0;
-                    int hours = 0; 
-                    int minutes = 0;
+            if (guildUser == null) { // the user whose id was given does not exist
+                // error
+                return;
+            }
 
+            if (guildUser.VoiceChannel == null) { // the user whose id was given is not in a vc
+                // error
+                return;
+            }
 
-                    foreach (string argument in arguments) {
-                        if (int.TryParse(argument.Substring(0, argument.Length - 1), out int numArgument)) {
-                            if (numArgument <= 0) {
-                                // error
-                                continue;
-                            }
+            int days = 0;
+            int hours = 0; 
+            int minutes = 0;
 
-                            char finalCharacter = argument[argument.Length - 1];
-
-                            switch (finalCharacter) {
-                                case 'w':
-                                    days += numArgument * 7;
-                                    break;
-
-                                case 'd':
-                                    days += numArgument;
-                                    break;
-
-                                case 'h':
-                                    hours += numArgument;
-                                    break;
-
-                                case 'm':
-                                    minutes += numArgument;
-                                    break;
-                            }
-                        }
-                    }
-
-                    TimeSpan difference = new TimeSpan(days, hours, minutes, 0);
-                    if (difference == TimeSpan.Zero) {
+            foreach (string argument in arguments) {
+                if (int.TryParse(argument[0..^1], out int numArgument)) {
+                    if (numArgument <= 0) { // a negative time span was given
                         // error
-                        return;
+                        continue;
                     }
 
-                    Server server = await Server.GetServerAsync(guild.Id);
-                    User serverUser = await server.GetUserAsync(user.Id);
-                    await serverUser.AddMutePersistedAsync(guildUser.VoiceChannel.Id, DateTime.UtcNow + difference);
+                    switch (argument[^1]) {
+                        case 'w':
+                            days += numArgument * 7;
+                            break;
 
-                    if (!guildUser.IsMuted) {
-                        server.TryAddVoiceStatusUpdated(guildUser.Id);
-                        await guildUser.ModifyAsync(userProperties => { userProperties.Mute = true; });
+                        case 'd':
+                            days += numArgument;
+                            break;
+
+                        case 'h':
+                            hours += numArgument;
+                            break;
+
+                        case 'm':
+                            minutes += numArgument;
+                            break;
                     }
                 }
+            }
+
+            TimeSpan difference = new TimeSpan(days, hours, minutes, 0);
+            if (difference == TimeSpan.Zero) { // no time was given OR all time that was given was 0
+                // error
+                return;
+            }
+
+            Server server = await guild.GetServerAsync();
+            User serverUser = await server.GetUserAsync(user.Id);
+            await serverUser.AddMutePersistedAsync(guildUser.VoiceChannel.Id, DateTime.UtcNow + difference);
+
+            if (!guildUser.IsMuted) {
+                server.TryAddVoiceStatusUpdated(guildUser.Id);
+                await guildUser.ModifyAsync(userProperties => { userProperties.Mute = true; });
             }
         }
     }

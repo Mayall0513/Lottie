96dff68708cd1e6a3d517d7fcbe38452dde18a7c
Mayall <mayall0513@outlook.com>
2021-10-25 00:40:44 +0100

    Added additional commands (give roles, mute)

---
+++ DiscordBot6/Commands/GiveRoles.cs
@@ -0,0 +1,38 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot6.Commands {
+    [Group("giveroles")]
+    public sealed class GiveRoles : ModuleBase<SocketCommandContext> {
+        [Command]
+        public async Task Command(ulong userId, params ulong[] roleIds) {
+            if (roleIds.Length == 0) {
+                await Context.Channel.SendMessageAsync($"You must put the IDs of the roles you want to add after the user's ID.");
+                return;
+            }
+
+            if (Context.User is SocketGuildUser socketGuildUser) {
+                Server server = await Context.Guild.GetServerAsync();
+                IEnumerable<ulong> userIds = socketGuildUser.Roles.Select(x => x.Id);
+
+                if (!await server.UserMayGiveRoles(socketGuildUser.Id, userIds)) {
+                    return;
+                }
+
+                User serverUser = await server.GetUserAsync(userId);
+                await serverUser.AddRolesPersistedAsync(roleIds);
+
+                SocketGuildUser guildUser = Context.Guild.GetUser(userId);
+                if (guildUser != null) {
+                    serverUser.IncrementRolesUpdated();
+                    await guildUser.AddRolesAsync(roleIds);
+                }
+            }
+        }
+    }
+}
---
+++ DiscordBot6/Commands/Mute.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot6.Commands {
+    [Group("mute")]
+    public sealed class Mute : ModuleBase<SocketCommandContext> {
+        [Command]
+        public async Task Command(ulong userId) {
+            if (Context.User is SocketGuildUser socketGuildUser) {
+                Server server = await Context.Guild.GetServerAsync();
+                IEnumerable<ulong> userIds = socketGuildUser.Roles.Select(x => x.Id);
+
+                if (!await server.UserMayGiveRoles(socketGuildUser.Id, userIds)) {
+                    return;
+                }
+
+                
+                User serverUser = await server.GetUserAsync(userId);
+                serverUser.GlobalMutePersisted = true;
+                await server.SetUserAsync(userId, serverUser);
+
+                SocketGuildUser guildUser = Context.Guild.GetUser(userId);
+                if (guildUser?.VoiceChannel != null && !guildUser.IsMuted) {
+                    serverUser.IncrementVoiceStatusUpdated();
+                    await guildUser.ModifyAsync(userProperties => { userProperties.Mute = true; });
+                }
+            }
+        }
+    }
+}

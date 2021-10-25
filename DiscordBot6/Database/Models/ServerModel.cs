30298fd8c194c70a612d8a47a389d0b1b09dde4a
Mayall <mayall0513@outlook.com>
2021-10-25 19:28:44 +0100

    Updated server model to reflect additions to server objects

--- DiscordBot6/Database/Models/ServerModel.cs
+++ DiscordBot6/Database/Models/ServerModel.cs
@@ -1,12 +1,20 @@
-﻿namespace DiscordBot6.Database.Models {
+﻿using System.Collections.Generic;
+
+namespace DiscordBot6.Database.Models {
     public sealed class ServerModel : IModelFor<Server> {
         public ulong Id { get; set; }
+
+        public char? CommandPrefix { get; set; }
+        public ulong? LogChannelId { get; set; }
+
         public bool AutoMutePersist { get; set; }
         public bool AutoDeafenPersist { get; set; }
         public bool AutoRolePersist { get; set; }
 
+        public HashSet<ulong> CommandChannels { get; set; } = new HashSet<ulong>();
+
         public Server CreateConcrete() {
-            return new Server(Id, AutoMutePersist, AutoDeafenPersist, AutoRolePersist);
+            return new Server(Id, CommandPrefix, LogChannelId, AutoMutePersist, AutoDeafenPersist, AutoRolePersist, CommandChannels);
         }
     }
 }

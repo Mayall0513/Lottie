d7dbe9195cdb320c96fbc6574e3c11157959529b
Mayall <mayall0513@outlook.com>
2021-10-29 09:41:08 +0100

    Switched over lists in models that contain ids to HashSets for automatic duplicate checking

--- DiscordBot6/Database/Models/Constraints/GenericConstraintModel.cs
+++ DiscordBot6/Database/Models/Constraints/GenericConstraintModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace DiscordBot6.Database.Models.Constraints {
     public sealed class GenericConstraintModel : IModelFor<GenericConstraint> {
         public bool Whitelist { get; set; }
-        public List<ulong> Requirements { get; } = new List<ulong>();
+        public HashSet<ulong> Requirements { get; } = new HashSet<ulong>();
 
         public GenericConstraint CreateConcrete() {
             return new GenericConstraint(Whitelist, Requirements);
--- DiscordBot6/Database/Models/Constraints/RoleConstraintModel.cs
+++ DiscordBot6/Database/Models/Constraints/RoleConstraintModel.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 namespace DiscordBot6.Database.Models.Constraints {
     public sealed class RoleConstraintModel : IModelFor<RoleConstraint> {
         public bool WhitelistStrict { get; set; }
-        public List<ulong> WhitelistRequirements { get; } = new List<ulong>();
+        public HashSet<ulong> WhitelistRequirements { get; } = new HashSet<ulong>();
 
         public bool BlacklistStrict { get; set; }
-        public List<ulong> BlacklistRequirements { get; } = new List<ulong>();
+        public HashSet<ulong> BlacklistRequirements { get; } = new HashSet<ulong>();
 
         public RoleConstraint CreateConcrete() {
             return new RoleConstraint(WhitelistStrict, WhitelistRequirements, BlacklistStrict, BlacklistRequirements);
